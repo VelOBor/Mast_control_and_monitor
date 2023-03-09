d1cbed647a31863bcb4a75b00c9207999bfa98ec
Oleg <57217654+VelOBor@users.noreply.github.com>
2023-03-09 12:47:55 +0300

    Uploaded entire project folder
    Still have the issue of lockout to contend with. When a locked axis is moved off-center, upon unlocking it immediately reads the value of the axis. Perhaps this will be a non-issue with mechanical spring-loaded joysticks.

--- src/mast_controller_and_monitor.cpp
+++ src/mast_controller_and_monitor.cpp
@@ -5,19 +5,28 @@ Hypothetical prototype designed with Fritzing and built on a breadboard
 The system is built around an Arduino NANO
 */
 
-//include arduino library for use in VSCode PlatformIO
-#include <Arduino.h>
+//==================include necessary libraries==================
+#include <Arduino.h> //include arduino library for use in VSCode PlatformIO (NOT NEEDED WHEN USING ARDUINO IDE!!!)
+#include <Wire.h> //include I2C library
+#include <INA219_WE.h> //include the INA library by Wolfgang Ewald
+#include <TM1637.h> //include the TM1637 library from hard drive (download from (https://drive.google.com/file/d/1DN5pDko7D1-F4POXICIfd8m_1sAKBZt7/view)
 
-//pin definitions
+#define I2C_ADDRESS 0x40 //define I2C address for the INA219 module
+
+//==================pin definitions==================
 
 //inputs
 int control_axis_1 = A0; //potentiometer joystick for vertical axis 1
 int control_axis_2 = A1; //potentiometer joystick for vertical axis 2
 int fingers_axis = A2;  //potentiometer joystick for "fingers" axis
-int pressure_sensor = A3; //currently simulated by a potentiometer
+
+//int pressure_sensor = A3; //currently simulated by a potentiometer, to be replaced by I2C INA219 module
+
+int neutral_switch_1 = 9; //neutral button within the joystick, neutral state is when the button is NOT pressed
+int neutral_switch_2 = 8; //neutral button within the joystick, neutral state is when the button is NOT pressed
+int neutral_switch_3 = 7; //neutral button within the joystick, neutral state is when the button is NOT pressed
 
 //outputs
-
 int up_state = 2; //status lights up LED when "up" command is received from any of the vertical axes (ON/OFF output)
 int down_state = 4; //status lights up LED when "down" command is received from any of the vertical axes (ON/OFF output)
 
@@ -28,7 +37,9 @@ int power_fingers = 3; //PWM output for closing power of "fingers" axis
 int display_clk = 12; //CLK pin of the TM1637 display module, perhaps rename later
 int display_dio = 13; //DIO pin of the TM1637 display module, perhaps rename later
 
-//variables
+//==================variables==================
+float current_mA = 0.0; //float variable for reading current from the INA module
+
 bool lock_1 = false; //flag for locking out axis 1
 bool lock_2 = false; //flag for locking out axis 2
 bool lock_3 = false; //flag for locking out fingers axis
@@ -36,6 +47,10 @@ bool lock_3 = false; //flag for locking out fingers axis
 bool axis_1_neutral = true; //flag for axis 1 being in neutral state
 bool axis_1_up = true; //flag for axis 1 being in UP state
 bool axis_1_down = true; //flag for axis 1 being in DOWN state
+
+int neutral_1_state = 0; //neutral 1 button state
+int neutral_2_state = 0; //neutral 2 button state
+int neutral_3_state = 0; //neutral 3 button state
 
 bool axis_2_neutral = true; //flag for axis 1 being in neutral state
 bool axis_2_up = true; //flag for axis 2 being in UP state
@@ -58,15 +73,28 @@ int axis_2_out = 0; //for storing output value mapped to appropriate PWM output
 int axis_fingers_val = 0; //for storing raw input values from the pots
 int axis_fingers_out = 0; //for storing output value mapped to appropriate PWM output
 
+//==================create instances of library objects==================
+INA219_WE ina219 = INA219_WE(I2C_ADDRESS); //create an instance of the INA I2C module
+TM1637 tm1637(display_clk, display_dio); //create an instance of the TM1637 module
 
 void setup() {
 Serial.begin(115200); //initialize serial communication
-
+Wire.begin(); //initialize the I2C interface
+  if(!ina219.init()){ //check if the INA module is online
+    Serial.println("INA219 not connected!");} //return error if the module is not responding
+tm1637.init(); //initialize the tm1637 module
+  tm1637.set(BRIGHT_TYPICAL); //set brightness level for the display module
 //pinmode definitions for inputs
 pinMode(control_axis_1, INPUT);
+pinMode(neutral_switch_1, INPUT_PULLUP);
+
 pinMode(control_axis_2, INPUT);
+pinMode(neutral_switch_2, INPUT_PULLUP);
+
 pinMode(fingers_axis, INPUT);
-pinMode(pressure_sensor, INPUT);
+pinMode(neutral_switch_3, INPUT_PULLUP);
+
+//pinMode(pressure_sensor, INPUT);
 
 //pinmode definitions for outputs
 pinMode(up_state, OUTPUT);
@@ -74,6 +102,10 @@ pinMode(down_state, OUTPUT);
 pinMode(display_clk, OUTPUT);
 pinMode(display_dio, OUTPUT);
 
+//reset lock status to FALSE for all three axes, probably get rid of this once buttons for neutral state are implemented
+lock_1 = false;
+lock_2 = false;
+lock_3 = false;
 
 }
 
@@ -97,16 +129,35 @@ if (axis_1_neutral == true){digitalWrite(power_1, LOW);}
 if (axis_2_neutral == true){digitalWrite(power_2, LOW);}
 if (axis_fingers_neutral == true){digitalWrite(power_fingers, LOW);}
 
-//==================read values if axis is not locked==================
+
+//==================get raw data from the pressure sensor==================
+current_mA = ina219.getCurrent_mA(); //read current from the INA module
+current_mA = constrain (current_mA, 0, 30); //limit the value IN SOFTWARE from 0 to 25mA for ease of calculations
+
+//==================convert data from float to int==================
+pressure_val = current_mA*10;
+
+//==================convert current from sensor to actual pressure value==================
+pressure_actual = map(pressure_val, 0, 300, 0, 250);
+
+//==================read values of axis is not locked==================
 axis_1_val = analogRead(control_axis_1);
 axis_2_val = analogRead(control_axis_2);
 axis_fingers_val = analogRead(fingers_axis);
 
-////==================read pressure sensor==================
-pressure_val = analogRead(pressure_sensor);
+//==================read neutral switches==================
+neutral_1_state = digitalRead(neutral_switch_1);
+neutral_2_state = digitalRead(neutral_switch_2);
+neutral_3_state = digitalRead(neutral_switch_3);
+
+
+//==================read pressure sensor==================
+//pressure_val = analogRead(pressure_sensor);
+//pressure_actual = map(pressure_val, 0, 1023, 0, 255);
 
 //==================process and map axis 1==================
-if (axis_1_val >= 562){
+
+    if (axis_1_val >= 562){
         axis_1_out = map(axis_1_val, 562, 1023, 0, 255);
         axis_1_neutral = false;
         axis_1_up = true;
@@ -121,9 +172,10 @@ if (axis_1_val >= 562){
         }
     if (axis_1_val > 462 && axis_1_val <= 562){axis_1_neutral = true, axis_1_up = false, axis_1_down = false, axis_1_out = 0;
     }    
-    
+
 //==================process and map axis 2==================
-if (axis_2_val >= 562){
+
+    if (axis_2_val >= 562){
         axis_2_out = map(axis_2_val, 562, 1023, 0, 255);
         axis_2_neutral = false;
         axis_2_up = true;
@@ -138,9 +190,10 @@ if (axis_2_val >= 562){
         }
     if (axis_2_val > 462 && axis_2_val <= 562){axis_2_neutral = true, axis_2_up = false, axis_2_down = false, axis_2_out = 0;
     }
-    
+
 //==================process and map axis fingers==================
-if (axis_fingers_val >= 562){
+
+    if (axis_fingers_val >= 562){
         axis_fingers_out = map(axis_fingers_val, 562, 1023, 0, 255);
         axis_fingers_neutral = false;
         axis_fingers_close = true;
@@ -155,9 +208,8 @@ if (axis_fingers_val >= 562){
         }
     if (axis_fingers_val > 462 && axis_fingers_val <= 562){axis_fingers_neutral = true, axis_fingers_close = false, axis_fingers_open = false, axis_fingers_out = 0;
     }
-    
-//activate appropriate status LED for up or down and output data to LED on PWM pins
 
+//activate appropriate status LED for up or down and output data to LED on PWM pins
 if (axis_1_neutral == true && lock_1 == false){digitalWrite(up_state, LOW), digitalWrite(down_state, LOW);}
 if (axis_2_neutral == true && lock_2 == false){digitalWrite(up_state, LOW), digitalWrite(down_state, LOW);}
 if (axis_fingers_neutral == true && lock_3 == false){digitalWrite(up_state, LOW), digitalWrite(down_state, LOW);}
@@ -170,6 +222,8 @@ if (axis_1_down == true && lock_1 == false){digitalWrite(up_state, LOW), digital
 if (axis_2_down == true && lock_2 == false){digitalWrite(up_state, LOW), digitalWrite(down_state, HIGH), analogWrite(power_2, axis_2_out);}
 if (axis_fingers_open == true && lock_3 == false){digitalWrite(up_state, LOW), digitalWrite(down_state, HIGH), analogWrite(power_fingers, axis_fingers_out);}
 
+tm1637.displayInt(pressure_actual);
+tm1637.clearDisplay();
 
 //serial debugging
 //output data on axis 1
@@ -191,7 +245,10 @@ Serial.print(" AFU: "); Serial.print(axis_fingers_close);
 Serial.print(" AFD: "); Serial.print(axis_fingers_open);
 Serial.print(" AFLock: "); Serial.print(lock_3);
 
+Serial.print(" Current[mA]:"); Serial.print(current_mA); //print current read from INA module
+
 Serial.print(" Pressure val:"); Serial.print(pressure_val);
+Serial.print(" Pressure actual:"); Serial.print(pressure_actual);
 
 Serial.println("  ");
 
