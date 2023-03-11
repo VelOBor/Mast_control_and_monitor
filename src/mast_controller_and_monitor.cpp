045ff0b1943d4bd8b042e13e37ad16ad7b5745b1
Oleg <57217654+VelOBor@users.noreply.github.com>
2023-03-11 20:20:31 +0300

    Translating comments to RU

--- src/mast_controller_and_monitor.cpp
+++ src/mast_controller_and_monitor.cpp
@@ -1,6 +1,6 @@
 /*
 Drilling mast control system V0.1 by Oleg "Salty NUggeTZ" Velichko and Igor "Terrano"
-Designed to control and monitor a hydraulic system of undisclosed origin and specs
+Designed to control and monitor a system of undisclosed origin and specs
 Hypothetical prototype designed with Fritzing and built on a breadboard
 The system is built around an Arduino NANO
 */
